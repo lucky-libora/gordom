7e9023c9e640bf929df684c7d84aceab7a4a39dc
Aleksey Vasilyev <luckylibora@gmail.com>
2020-04-19 00:59:02 +0300

    improved README.md small refactoring

--- README.md
+++ README.md
@@ -37,20 +37,37 @@ import (
     "github.com/lucky-libora/gordom"
 )
 
+// gordom maps dom data to struct fields'
+// css query passed by $ tag 
 type GitHubProjectFile struct {
-    Link string `value:"[href]"`
+    // by default field value is set from a text of html node
     Name string
+    // also it can be set from an attribute value by adding tag value:"[attribute_name]"
+    Link string `value:"[href]"`
+}
+
+type GitHubProjectInfo struct {
+    // get node by the attribute itemprop='about' and set node's text to the field
+    Description string `$:"[itemprop='about']"`
+    // int, uint, float types are automatically converted
+    Commits     int8   `$:".commits .num"`
 }
 
 type GitHubProject struct {
+    // structures are also supported. You can select a parent node for inner structure if it necessary
+    Info GitHubProjectInfo `$:".repository-content "`
+    // you can iterate even arrays
     Files []GitHubProjectFile `$:".js-navigation-item > .content a"`
+    // WARNING: pointers can't be used as field types
 }
 
 func main() {
     project := &GitHubProject{}
-    err := gordom.ParseFromUrl("https://github.com/lucky-libora/go-parse-it", project)
+    // pass pointer to your structure. Gordom will fill all fields
+    err := gordom.ParseFromUrl("https://github.com/lucky-libora/gordom", project)
     if err != nil {
         fmt.Println(err)
+        return
     }
     fmt.Println(project)	
 }
--- parser.go
+++ parser.go
@@ -40,49 +40,74 @@ func findOne(node *node.Node, typeField reflect.StructField) *node.Node {
 	return node
 }
 
+func parseFloat(value string, valueField reflect.Value) error {
+	f, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return err
+	}
+	valueField.SetFloat(f)
+	return nil
+}
+
+func parseInt(value string, valueField reflect.Value) error {
+	i, err := strconv.ParseInt(value, 10, 64)
+	if err != nil {
+		return err
+	}
+	valueField.SetInt(i)
+	return nil
+}
+
+func parseSlice(node *node.Node, typeField reflect.StructField, valueField reflect.Value) error {
+	slice := reflect.MakeSlice(typeField.Type, 1, 1)
+	sliceType := slice.Index(0).Type()
+	slice = reflect.MakeSlice(typeField.Type, 0, 0)
+	found := findMany(node, typeField)
+	for _, n := range found {
+		value, err := createNewStruct(n, sliceType)
+		if err != nil {
+			return err
+		}
+		slice = reflect.Append(slice, value)
+	}
+	valueField.Set(slice)
+	return nil
+}
+
+func parseStruct(node *node.Node, typeField reflect.StructField, valueField reflect.Value) error {
+	str, err := createNewStruct(node, typeField.Type)
+	if err != nil {
+		return err
+	}
+	valueField.Set(str)
+	return nil
+}
+
+func parseUint(value string, valueField reflect.Value) error {
+	i, err := strconv.ParseUint(value, 10, 64)
+	if err != nil {
+		return err
+	}
+	valueField.SetUint(i)
+	return nil
+}
+
 func parseValue(node *node.Node, typeField reflect.StructField, valueField reflect.Value) error {
 	kind := typeField.Type.Kind()
 	switch kind {
 	case reflect.Slice:
-		slice := reflect.MakeSlice(typeField.Type, 1, 1)
-		sliceType := slice.Index(0).Type()
-		slice = reflect.MakeSlice(typeField.Type, 0, 0)
-		found := findMany(node, typeField)
-		for _, n := range found {
-			value, err := createNewStruct(n, sliceType)
-			if err != nil {
-				return err
-			}
-			slice = reflect.Append(slice, value)
-		}
-		valueField.Set(slice)
+		return parseSlice(node, typeField, valueField)
 	case reflect.Struct:
-		str, err := createNewStruct(node, typeField.Type)
-		if err != nil {
-			return err
-		}
-		valueField.Set(str)
+		return parseStruct(node, typeField, valueField)
 	default:
 		value := getValue(findOne(node, typeField), typeField)
 		switch kind {
 		case reflect.Float32, reflect.Float64:
-			f, err := strconv.ParseFloat(value, 64)
-			if err != nil {
-				return err
-			}
-			valueField.SetFloat(f)
+			return parseFloat(value, valueField)
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			i, err := strconv.ParseInt(value, 10, 64)
-			if err != nil {
-				return err
-			}
-			valueField.SetInt(i)
+			return parseInt(value, valueField)
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			i, err := strconv.ParseUint(value, 10, 64)
-			if err != nil {
-				return err
-			}
-			valueField.SetUint(i)
+			return parseUint(value, valueField)
 		case reflect.String:
 			valueField.SetString(value)
 		}
